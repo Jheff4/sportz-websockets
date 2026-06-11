46e2cb7126aa7b822430a605a2b3ae1c7c6dfaf3
Jheff4 <ogbevoenetinosa@gmail.com>
2026-06-11 12:16:43 +0100

    feat: added schema and db

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
 const PORT = 8000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/', (_req: Request, res: Response) => {
   res.json({ message: 'Sportz server is running!' });
 });
 
