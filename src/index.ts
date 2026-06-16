9050414732b3ae0d0dbd888294ff573365fa3279
Jheff4 <ogbevoenetinosa@gmail.com>
2026-06-16 18:02:00 +0100

    feat: added site 24/7 for observability

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import AgentAPI from 'apminsight';
+AgentAPI.config()
+
 import express, { Request, Response } from 'express';
 import http from 'http';
 import { matchRouter } from './routes/matches.js';
